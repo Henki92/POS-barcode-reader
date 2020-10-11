6382f04e325e3bc7238b12566229a68d110b30b1
Henki92 <37691611+Henki92@users.noreply.github.com>
2020-10-11 14:14:22 +0200

    Add files via upload

---
+++ barcodeReader.bat
@@ -0,0 +1,2 @@
+cd %cd%
+pyinstaller --onefile --noconsole --icon=POS_2.ico --clean --name streckkodslasare_med_nätverk main_network.py
---
+++ barcodeReaderlokal.bat
@@ -0,0 +1,2 @@
+cd %cd%
+pyinstaller --onefile --noconsole --icon=POS_2.ico --clean --name streckkodslasare_utan_netverk main_lokal.py
